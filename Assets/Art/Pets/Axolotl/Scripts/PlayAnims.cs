5695dfb1edf4d72e2996e11ebe4e50f6132d0191
杨皓文 <steven@macbook.m3>
2024-07-15 23:43:46 +0800

    UI Assets Import，怪物导入

---
+++ Assets/Art/Pets/Axolotl/Scripts/PlayAnims.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace deko
+{
+
+public class PlayAnims : MonoBehaviour
+{
+	public List<string> mAnimNames = new List<string>();
+	public Animator mAnimator;
+
+	void Start()
+	{
+		if (mAnimator == null) mAnimator = gameObject.GetComponent<Animator>();
+	}
+
+	public void OnButtonClick( int aIndex )
+	{
+		if (mAnimator == null) mAnimator = gameObject.GetComponent<Animator>();
+		mAnimator.Play( mAnimNames[ aIndex ] );
+	}
+
+	public void OnButtonClickWithAnimName( string aAnimStateName )
+	{
+		if (mAnimator == null) mAnimator = gameObject.GetComponent<Animator>();
+		mAnimator.Play( aAnimStateName );
+	}
+
+}
+
+}
