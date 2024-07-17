9bbbf3555e4db8c06b6a144bacd37cb65c95e7dc
祝浩淳 <kuchinashi@ahympia.com>
2024-07-17 17:51:28 +0800

    位置记录与还原

--- Assets/Script/Game/MainController.cs
+++ Assets/Script/Game/MainController.cs
@@ -2,13 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DataSystem;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MainController : MonoBehaviour
 {
+    public UnityEvent Initialization;
+    public UnityEvent FirstTimeEnterGame;
+    
     private void Awake()
     {
+        Initialization?.Invoke();
+        
         RecoverPose();
+    }
+
+    private void OnApplicationQuit()
+    {
+        GameProgressData.Instance.LastLoginTime = DateTime.Now;
+        GameProgressData.Instance.Serialization();
     }
 
     public void RecordPose(XenoSDK.BuildingBlocks.GrabPlace.Pose pose)
@@ -19,11 +32,15 @@ public class MainController : MonoBehaviour
     
     public void RecoverPose()
     {
-        if (GameProgressData.Instance.WorkbenchPose == null) return;
+        if (GameProgressData.Instance.WorkbenchPose == null)
+        {
+            FirstTimeEnterGame?.Invoke();
+            return;
+        }
         
         transform.position = GameProgressData.Instance.WorkbenchPose.Position;
         transform.rotation = GameProgressData.Instance.WorkbenchPose.Rotation;
         
-        gameObject.SetActive(true);
+        transform.Find("Workbench").gameObject.SetActive(true);
     }
 }
